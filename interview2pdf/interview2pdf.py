00a925e55528fabeee45ac8d70a5450d2dbada2f
Alexandr Pinaev <sasha.pinaev@outlook.com>
2016-11-16 04:21:56 +0300

    Update interview2pdf.py
    params parse

--- interview2pdf/interview2pdf.py
+++ interview2pdf/interview2pdf.py
@@ -5,32 +5,43 @@ import argparse
 import interview2csv
 import csv2pdf
 
+helpTxt = "\n"
+helpTxt += "interview2pdf converter \n"
+helpTxt += "please, provide to script following params \n"
+helpTxt += "(in any order | pattern: param=value | value without quotas) \n \n"
+helpTxt += "	pathFrom - path to interview file, !required \n \n"
+helpTxt += "	pathTo - path+name to pdf file, default: ./result.pdf \n \n"
+helpTxt += "	type - 'common' for data with common questions \n 		or 'full' for data with questions about disciplines, optional, default: full. \n \n"
+helpTxt += "	comments - path to file with comment-questions labels, optional, default: full. \n \n"
+
 parser = argparse.ArgumentParser()
-parser.add_argument("pathFrom", type=str, help="name of csv")
-parser.add_argument("pathTo", type=str, help="name of pdf")
-parser.add_argument("type", type=str, help="full / common", nargs='?')
-parser.add_argument("chartType", type=str, help="pie / bar", nargs='?')
-parser.add_argument("comments", type=str, help="file with comments", nargs='?')
-args = parser.parse_args()
-
-if (not args.chartType):
-    args.chartType = 'pie'
-if (not args.type):
-    args.type = 'full'
-if (args.comments):
-    path = args.comments
-    text = open(path)
-    args.comments = []
-    for line in text:
-        if line!='\n':
-            args.comments += [line.decode('utf8').strip("\n'")]
-    text.close()
+parser.add_argument("args", type=str, help=helpTxt, nargs='*')
+argsN = parser.parse_args()
+args = {}
+for arg in argsN.args:
+	arg = arg.split('=')
+	args[arg[0]]=arg[1]
+if not('pathFrom' in args):
+	print helpTxt
 else:
-    args.comments = [u'Оставьте комментарий:']
+	if not('chartType' in args):
+		args['chartType'] = 'pie'
+	if not('type' in arg):
+		args['type'] = 'full'
+	if not('comments' in args):
+		path = args['comments']
+		text = open(path)
+		args['comments'] = []
+		for line in text:
+			if line!='\n':
+				args['comments'] += [line.decode('utf8').strip("\n'")]
+		text.close()
+	else:
+		args['comments'] = [u'Оставьте комментарий:']
 
-if (args.type == 'full'):
-    csvData = interview2csv.parseInterviewDataRaw(interview2csv.readDataRaw(args.pathFrom), args.comments) 
-    csv2pdf = csv2pdf.csv2pdf(csvData, args.pathTo, args.chartType)
-elif (args.type == 'common'):
-    csvData = interview2csv.parseInterviewDataRawCommon(interview2csv.readDataRaw(args.pathFrom), args.comments) 
-    csv2pdf = csv2pdf.csvCommon2pdf(csvData, args.pathTo)
+	if (args['type'] == 'full'):
+		csvData = interview2csv.parseInterviewDataRaw(interview2csv.readDataRaw(args['pathFrom']), args['comments']) 
+		csv2pdf = csv2pdf.csv2pdf(csvData, args['pathTo'], args['chartType'])
+	elif (args['type'] == 'common'):
+		csvData = interview2csv.parseInterviewDataRawCommon(interview2csv.readDataRaw(args['pathFrom']), args['comments']) 
+		csv2pdf = csv2pdf.csvCommon2pdf(csvData, args['pathTo'])
